6edbaea75265e40c01a99c6539afde4a43dd2d36
Chakradhar Rao <chakradhar.jonagam@biqmind.com>
2020-07-06 14:55:47 -0700

    update

--- tryCAPE/install-cape/setup.sh
+++ tryCAPE/install-cape/setup.sh
@@ -1,2 +1,3 @@
 #!/bin/bash -x
 kubectl apply -f https://raw.githubusercontent.com/containous/traefik/v1.7/examples/k8s/traefik-deployment.yaml
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
