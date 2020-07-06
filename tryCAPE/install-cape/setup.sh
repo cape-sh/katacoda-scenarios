206036ad6b0d83c4eb8a3967e011057b60f1adb4
Chakradhar Rao <chakradhar.jonagam@biqmind.com>
2020-07-06 16:10:11 -0700

    update

--- tryCAPE/install-cape/setup.sh
+++ tryCAPE/install-cape/setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -x
-kubectl apply -f /root/ingress-install.yaml
+kubectl apply -f https://gist.githubusercontent.com/debianmaster/3db1b947e1401d49e700b9aabaf5f8d9/raw/da1f59fca35bfe1f98e8b83ddd1debaeeb579735/traefik-nodeport.yaml
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+clear
--- tryCAPE/install-cape/step2.md
+++ tryCAPE/install-cape/step2.md
@@ -2,5 +2,5 @@
 
 Enter the following command
 ```
-kubectl -n cape set env API_BASE_URL=[[HOST_SUBDOMAIN]]-30036-[[KATACODA_HOST]].environments.katacoda.com
+kubectl -n cape set env API_BASE_URL=https://[[HOST_SUBDOMAIN]]-30036-[[KATACODA_HOST]].environments.katacoda.com
 ```{{execute HOST1}}
